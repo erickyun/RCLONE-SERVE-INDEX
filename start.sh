0bb4f27d2c8dfd0082a4a8aec03ff835a03d08c5
erickyun <71968711+erickyun@users.noreply.github.com>
2023-12-31 18:33:48 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-rclone serve webdav AllDrives: --addr 0.0.0.0:3000
+curl "$CONFIG_IN_URL"> /home/rc.conf; rclone serve webdav AllDrives: --addr 0.0.0.0:3000 --user $INDEXusername --pass $INDEXpassword --config=/home/rc.conf
