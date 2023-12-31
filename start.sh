29a663b4f98ff715d23ad35b66a42ea6f27f8d4f
erickyun <71968711+erickyun@users.noreply.github.com>
2023-12-31 20:10:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-curl "$CONFIG_IN_URL" > /home/rc.conf; rclone serve webdav $CLOUDNAME: --addr :$PORT --user $INDEXusername --pass $INDEXpassword --config=/home/rc.conf
+curl -L "$CONFIG_IN_URL" -o /home/rc.conf; rclone serve webdav $CLOUDNAME: --addr :$PORT --user $INDEXusername --pass $INDEXpassword --config=/home/rc.conf
